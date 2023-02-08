cb7fe64692170f03fb54eff3a18620b87a91aa25
Pratik1503 <90402439+Pratik1503@users.noreply.github.com>
2023-02-08 11:44:24 +0530

    initial commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,443 @@
+import "./Landing.css";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <div className="footer">
+        <div className="sectionsfooterdark">
+          <div className="sectionsfooterdark">
+            <img className="bg-icon" alt="" src="../bg.svg" />
+            <div className="subscribe">
+              <div className="keep-up-to">
+                Keep up to date with all new themes and updates.
+              </div>
+              <input
+                className="call"
+                type="email"
+                
+                placeholder="email"
+                required
+              />
+              <div className="subscribe1">Subscribe</div>
+            </div>
+            <div className="links">
+              <div className="about-company">About company</div>
+              <div className="our-services">Our services</div>
+              <div className="team-members">Team members</div>
+              <div className="portfolio">Portfolio</div>
+              <div className="pricing-plans">Pricing plans</div>
+              <div className="testimonials">Testimonials</div>
+              <div className="contact">Contact</div>
+              <div className="links1">Links</div>
+            </div>
+            <div className="contacts">
+              <div className="address-2-st">
+                Address: 2 St Gabriel Walk, London SE1 6FG, United Kingdom
+              </div>
+              
+              <div className="e-mail-infobetria">E-mail: info@betria.com</div>
+              <div className="contacts1">Contacts</div>
+              <button className="call1" autoFocus>
+                <div className="text">
+                  <div className="have-questions">Have questions?</div>
+                  <b className="b">+44 141 552 3000</b>
+                </div>
+                <div className="headset"></div>
+              </button>
+            </div>
+          </div>
+          <div className="elipso-2019-landi">
+            © Elipso 2019. Landing Page Template by merkulove
+          </div>
+          <div className="rectangle" />
+        </div>
+      </div>
+      <div className="support">
+        <div className="div">
+          <div className="question-circle"></div>
+          <div className="looking-for-support">Looking for support?</div>
+          <div className="on-the-other">
+            On the other hand, we denounce with righteous dislike so men who are
+            beguiled indignation and they foresee.
+          </div>
+          <button className="linkviolet" autoFocus>
+            <div className="link">Read more</div>
+            <div className="long-arrow-right"></div>
+          </button>
+        </div>
+        <div className="div1">
+          <div className="question-circle"></div>
+          <div className="looking-for-support">View common questions</div>
+          <div className="on-the-other">
+            Et harum quidem rerum facilis est et expedita distinctio. Nam libero
+            tempore, cum soluta nobis est eligendi.
+          </div>
+          <button className="linkviolet" autoFocus>
+            <div className="link">Read more</div>
+            <div className="long-arrow-right"></div>
+          </button>
+        </div>
+      </div>
+      <div className="price">
+        <div className="sectionsfooterdark">
+          <div className="sectionsfooterdark">
+            <div className="div2">
+              <div className="sectionsfooterdark">
+                <div className="bg" />
+                <div className="rectangle1" />
+              </div>
+              <div className="text1">
+                <div className="per-month">Per month</div>
+                <div className="free-updates">Free updates</div>
+                <div className="check"></div>
+                <div className="custom-domain">Custom domain</div>
+                <div className="check1"></div>
+                <div className="outstanding-support">Outstanding support</div>
+                <div className="check2"></div>
+                <div className="unlimited-storage">Unlimited storage</div>
+                <div className="times"></div>
+                <div className="build-responsive-web">
+                  Build responsive website
+                </div>
+                <div className="times1"></div>
+                <b className="basic">Basic</b>
+                <b className="b1">15$</b>
+              </div>
+              <div className="button">
+                <button className="buttonnormalgradientviolet" >
+                  <button className="bg1"  />
+                  <div className="button1">Getting started</div>
+                </button>
+              </div>
+            </div>
+            <div className="div3">
+              <div className="sectionsfooterdark">
+                <div className="bg" />
+                <div className="rectangle1" />
+              </div>
+              <div className="text2">
+                <div className="per-month1">Per month</div>
+                <div className="free-updates1">Free updates</div>
+                <div className="check3"></div>
+                <div className="custom-domain1">Custom domain</div>
+                <div className="check4"></div>
+                <div className="outstanding-support1">Outstanding support</div>
+                <div className="check5"></div>
+                <div className="unlimited-storage1">Unlimited storage</div>
+                <div className="times2"></div>
+                <div className="build-responsive-web1">
+                  Build responsive website
+                </div>
+                <div className="times3"></div>
+                <b className="standard">Standard</b>
+                <b className="b2">37$</b>
+              </div>
+              <div className="button2">
+                <button className="buttonnormalgradientviolet1" autoFocus>
+                  <button className="bg3" autoFocus />
+                  <div className="button3">Getting started</div>
+                </button>
+              </div>
+            </div>
+            <div className="primary">
+              <div className="sectionsfooterdark">
+                <div className="bg" />
+                <div className="rectangle1" />
+              </div>
+              <div className="text1">
+                <div className="per-month2">Per month</div>
+                <div className="free-updates">Free updates</div>
+                <div className="check"></div>
+                <div className="custom-domain">Custom domain</div>
+                <div className="check1"></div>
+                <div className="outstanding-support">Outstanding support</div>
+                <div className="check2"></div>
+                <div className="unlimited-storage2">Unlimited storage</div>
+                <div className="times4"></div>
+                <div className="build-responsive-web2">
+                  Build responsive website
+                </div>
+                <div className="times5"></div>
+                <b className="advance">Advance</b>
+                <b className="b3">42$</b>
+              </div>
+              <div className="button">
+                <button className="buttonnormalgradientviolet" >
+                  <button className="bg1"  />
+                  <div className="button1">Getting started</div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="badge">
+            <div className="rectangle4" />
+            <b className="best-choise">{`best choise `}</b>
+          </div>
+        </div>
+      </div>
+      <div className="call-to-action">
+        <img className="bg-icon" alt="" src="../bg1.svg" />
+        <div className="txt">
+          <div className="build-your-successfu">
+            Build your successful business with us. Want to know how to do this?
+          </div>
+        </div>
+        <div className="button6">
+          <button className="buttonnormalstokedefault" autoFocus>
+            <button className="bg6" autoFocus />
+            <div className="button7">Contact us</div>
+          </button>
+        </div>
+      </div>
+      <div className="skills">
+        <div className="skill-bar">
+          <div className="prmarketing">
+            <div className="bar-progress" />
+            <div className="bar-filled" />
+            <div className="pr-marketing">{`PR & Marketing`}</div>
+            <div className="div4">90%</div>
+          </div>
+          <div className="prmarketing1">
+            <div className="bar-progress" />
+            <div className="bar-filled1" />
+            <div className="pr-marketing">{`PR & Marketing`}</div>
+            <div className="div4">88%</div>
+          </div>
+          <div className="designdevelopment">
+            <div className="bar-progress" />
+            <div className="bar-filled2" />
+            <div className="pr-marketing">{`Design & Development`}</div>
+            <div className="div4">81%</div>
+          </div>
+          <div className="branding">
+            <div className="bar-progress" />
+            <div className="bar-filled3" />
+            <div className="pr-marketing">Branding</div>
+            <div className="div4">96%</div>
+          </div>
+        </div>
+        <div className="text4">
+          <b className="our-skills">{`Our skills & expertise`}</b>
+          <div className="we-help-company">
+            We help company set up business strategy including design brand
+            identiy or product design service. So company can focus on their
+            core business. Morbi accumsan ipsum velit. Nam nec tellus.
+          </div>
+          <button
+            className="buttonnormalgradientviolet2"
+            autoFocus
+            value="We are in progress"
+          >
+            <button className="bg7" autoFocus />
+            <div className="button8">We are in the progress</div>
+          </button>
+        </div>
+      </div>
+      <div className="services">
+        <div className="sectionsfooterdark">
+          <div className="div8">
+            <div className="bg" />
+            <b className="corporate-finance">Corporate finance</b>
+            <div className="usd-circle"></div>
+            <div className="lorem-ipsum-dolor">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed …
+            </div>
+          </div>
+          <div className="div9">
+            <div className="bg" />
+            <b className="corporate-finance">{`Audit & Evaluation`}</b>
+            <div className="usd-circle"></div>
+            <div className="lorem-ipsum-dolor">
+              Cras risus arcu, sodales ac, ultrices ac, mollis quis, justo. Sed
+              a libero. …
+            </div>
+          </div>
+          <div className="hover">
+            <div className="bg10" />
+            <b className="market-analysis">Market analysis</b>
+            <div className="nibh-vel-velit">
+              Nibh vel velit auctor aliqunean sollicitudinlorem bibendum auci …
+            </div>
+            <button className="linkwhite" autoFocus>
+              <div className="read-more">Read more</div>
+              <div className="long-arrow-right2"></div>
+            </button>
+          </div>
+          <div className="div10">
+            <div className="bg" />
+            <b className="corporate-finance">{`Sales & Trade`}</b>
+            <div className="usd-circle"></div>
+            <div className="lorem-ipsum-dolor">
+              Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
+              Morbi …
+            </div>
+          </div>
+          <div className="text5">
+            <b className="get-a-solutions">
+              Get a solutions to your businesses
+            </b>
+            <div className="our-consultants-help">
+              Our consultants help address your most challenging issues by
+              providing strategic, financial, operational, and IT services.
+            </div>
+            <div className="buttonnormalgradientviolet3">
+              <div className="button9">All services</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="split-section">
+        <img className="img-icon" alt="" src="../img.svg" />
+        <div className="text6">
+          <b className="we-offer-a">
+            We offer a full range of digital marketing services
+          </b>
+          <div className="latin-words-combine">
+            Latin words, combined with a handful of model sentence structures,
+            to generate Lorem Ipsum which looks reasonable. The generated Lorem
+            Ipsum is therefore always free.
+          </div>
+          <button className="buttonnormalgradientviolet4" >
+            <button className="bg12" />
+            <div className="button10">View prices</div>
+          </button>
+          <button className="linkviolet2">
+            <div className="link">More info</div>
+            <div className="long-arrow-right3"></div>
+          </button>
+        </div>
+      </div>
+      <div className="video">
+        <img className="bg-icon" alt="" src="../background.svg" />
+        <div className="text7">
+          <b className="how-it-works">How it works</b>
+          <div className="this-app-does">
+            This app does everything you could want it to do it is beautifully
+            and extremely intuitive to use.
+          </div>
+        </div>
+        <img className="play-button-icon" alt="" src="../playbutton.svg" />
+      </div>
+      <div className="split-section1">
+        <img className="img-icon1" alt="" src="../img1.svg" />
+        <div className="text8">
+          <b className="get-the-ultimate">
+            Get the ultimate tool and learn how to grow your audience
+          </b>
+          <div className="at-vero-eos">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque. Many desktop
+            publishing packages and web.
+          </div>
+          <button className="buttonnormalgradientviolet5">
+            <button className="bg3" />
+            <div className="button11">Get started</div>
+          </button>
+          <button className="linkviolet3">
+            <div className="link">Read more</div>
+            <div className="long-arrow-right"></div>
+          </button>
+        </div>
+      </div>
+      <div className="services2">
+        <div className="div2">
+          <div className="bg" />
+          <b className="corporate-finance">Corporate finance</b>
+          <div className="usd-circle"></div>
+          <div className="leverage-agile-frame">
+            Leverage agile frameworks to provide a robust synopsis
+          </div>
+        </div>
+        <div className="div3">
+          <div className="bg" />
+          <b className="corporate-finance">{`Audit & Evaluation`}</b>
+          <div className="usd-circle"></div>
+          <div className="leverage-agile-frame">
+            Bring to the table win-win survival strategies to ensure proactive
+          </div>
+        </div>
+        <div className="hover1">
+          <div className="bg10" />
+          <b className="market-analysis">Market analysis</b>
+          <div className="nibh-vel-velit1">
+            Nibh vel velit auctor aliqunean sollicitudinlorem bibendum auci …
+          </div>
+          <button className="linkwhite">
+            <div className="read-more">Read more</div>
+            <div className="long-arrow-right2"></div>
+          </button>
+        </div>
+      </div>
+      <div className="hero">
+        <div className="image">
+          <div className="shape" />
+          <img className="asset-14x-icon" alt="" src="../asset-14x@2x.png" />
+        </div>
+        <form className="txt1">
+          <div className="down">
+          <button className="linkviolet4">
+            <div className="link">Order similar</div>
+            <div className="long-arrow-right6"></div>
+          </button>
+          <button className="buttonnormalwhitedefault">
+            <button className="bg3 button11">Contact us</button>
+            
+          </button>
+          </div>
+          <b className="realize-your-full">Realize your full potential</b>
+          <div className="we-help-our">
+            We help our clients make realize their most important business
+            goals.
+          </div>
+          <div className="scroll-down">
+            <img className="bg-icon" alt="" src="../circle.svg" />
+            <div className="long-arrow-down"></div>
+          </div>
+        </form>
+      </div>
+      <div className="header">
+        <div className="sectionsfooterdark">
+          <div className="line" />
+          <div className="menu">
+            <div className="hover2">
+              <b className="home">Home</b>
+              <div className="line1" />
+            </div>
+            <div className="pages">Pages</div>
+            <div className="services3">Services</div>
+            <div className="blog">Blog</div>
+            <div className="contact1">Contact</div>
+            <div className="portfolio1">Portfolio</div>
+          </div>
+          <img className="logo-icon" alt="" src="../logo.svg" />
+          <div className="button13">
+            <div className="buttonnormalstokedefault1">
+              <div className="bg18" />
+              <div className="button14">Login</div>
+            </div>
+          </div>
+          <div className="button15">
+           <button className="bg6 button16">Create account</button>    
+
+          </div>
+          <div className="trendy-theme-for">
+            Trendy theme for companies, agencies and startups
+          </div>
+          <div className="div13">+44 141 552 3000</div>
+          <div className="envelope"></div>
+          <div className="or">or</div>
+        </div>
+      </div>
+      <div className="dropdowns">
+        <div className="bg" />
+        <div className="our-team">Our team</div>
+        <b className="team-member">Team member</b>
+        <div className="pricing">Pricing</div>
+        <div className="testimonials1">Testimonials</div>
+        <div className="page">404 page</div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
